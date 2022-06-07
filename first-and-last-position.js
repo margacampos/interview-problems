fa8461628e07a4bf616a0e1aad1b326be6e771ab
margacampos <margacampos@hotmail.es>
2022-06-07 21:41:07 +0100

    Binary implementation

--- first-and-last-position.js
+++ first-and-last-position.js
@@ -1,6 +1,26 @@
 const arr = [ 1, 2, 2, 2, 4, 7, 7, 9, 10, 10, 10, 11 ];
 const target = 10;
 
+// Without binary :
+const firstLastPosition = ( arr, target ) => {
+    let start = arr.indexOf(target);
+    
+    if(start === -1)return [-1, -1];
+    let end = start;
+    
+    // O(n)
+    for ( let i = start; i < arr.length; i++ ){
+        if( arr[i] !== target )break;
+        if(i>end)end=i;
+    }
+    
+    return [ start, end ];
+};
+
+console.log(firstLastPosition(arr, target));
+
+// With binary
+
 const findFirstTarget = ( arr, start, end, target ) => {
     const mid = Math.floor( ( start + end ) / 2 );
     
@@ -15,24 +35,26 @@ const findFirstTarget = ( arr, start, end, target ) => {
     
 };
 
-const firstLastPosition = ( arr, target ) => {
-    // let start = arr.indexOf(target);
-    let start = findFirstTarget( arr, 0, arr.length - 1, target );
+const findLastTarget = ( arr, start, end, target ) => {
+    const mid = Math.floor( ( start + end ) / 2 );
+    
+    if( arr[mid] === target ) {
+        if (!arr[mid + 1] || arr[mid + 1] > arr[mid])return mid;
+    };
 
-    if(start === -1)return [-1, -1];
-    let end = start;
+    if( end - start === 0 ) return -1;
 
-    // O(n)
-    for ( let i = start; i < arr.length; i++ ){
-        if( arr[i] !== target )break;
-        if(i>end)end=i;
-    }
-
-    return [ start, end ];
+    if( arr[mid] > target ) return findLastTarget( arr, start, mid - 1, target );
+    else return findLastTarget( arr, mid + 1, end, target );
+    
 };
 
-console.log(firstLastPosition(arr, target));
+const firstLastPositionBinary = ( arr, target ) => {
+    let start = findFirstTarget( arr, 0, arr.length - 1, target );
+    let last = findLastTarget( arr, 0, arr.length - 1, target );
+
+    return [ start, last ];
+};
 
 
-
-// console.log(findFirstTarget( arr, 0, arr.length - 1, target ));
+console.log(firstLastPositionBinary( arr, target ));
